a01e644513b5b6432d50b63e59bb6a3267d1e90f
AbdusattorYaminjonov <abdusattoryaminjonov@gmail.com>
2023-03-17 14:16:24 +0500

    update

---
+++ navbar.php
@@ -0,0 +1,31 @@
+    <!-- Navbar Start -->
+    <div class="container-fluid position-relative nav-bar p-0">
+        <div class="container position-relative" style="z-index: 9;">
+            <nav class="navbar navbar-expand-lg bg-secondary navbar-dark py-3 py-lg-0 pl-3 pl-lg-5">
+                <a href="index.php" class="navbar-brand">
+                    <h1 class="m-0 display-5 text-white"><span class="text-primary">Rent</span>HOUSE</h1>
+                </a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto py-0">
+                        <a href="index.php" class="nav-item nav-link active">Bosh sahifa</a>
+                         <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Ijaraga beriladigan Uylar</a>
+                            <div class="dropdown-menu rounded-0 m-0">
+                                <a href="blog.php" class="dropdown-item">Hovli uy</a>
+                                <a href="blog.php" class="dropdown-item">Ko'ngilochar uylar</a>
+                                <a href="blog.php" class="dropdown-item">Ko'pqavatli uy</a>
+                                <a href="blog.php" class="dropdown-item">Do'm/Kvartira</a>
+                                <a href="blog.php" class="dropdown-item">Katej uy</a>
+                            </div>
+                        </div>
+                        <a href="service.php" class="nav-item nav-link">Biz haqimizda</a>
+                        <a href="contact.php" class="nav-item nav-link">Aloqa</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
